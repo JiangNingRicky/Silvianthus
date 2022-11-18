0929d4132cf8f6120ce204e81757d2a37608e604
Ricky <ricky.jiang@valtech.com>
2022-11-19 00:40:24 +0800

    1. Multi-threads 2. Directory filter

--- src/main/java/com/expert/utils/FileUtils.java
+++ src/main/java/com/expert/utils/FileUtils.java
@@ -6,8 +6,10 @@ import com.itextpdf.kernel.pdf.canvas.parser.PdfTextExtractor;
 import java.io.File;
 import java.io.IOException;
 import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
 
 public class FileUtils {
@@ -16,36 +18,50 @@ public class FileUtils {
     private static AtomicLong totalFile = new AtomicLong();
     private static AtomicLong totalFileRead = new AtomicLong();
 
-    public static void main(String[] args) {
+    private static ThreadPoolExecutor threadPoolExecutor =
+            new ThreadPoolExecutor(20,20,30,TimeUnit.SECONDS,new ArrayBlockingQueue(1000));
+
+    private static CountDownLatch totalFileCountdownLatch = new CountDownLatch(0);
+
+    public static void main(String[] args) throws InterruptedException {
         Long startTime = System.currentTimeMillis();
         //先统计一遍文件总数，用于监控进度
         letterCountDir("/Users/valtechwh/Documents/N132","pdf",true);
         System.out.println("--------------------------【START】There are ["+totalFile+"] files to be read");
+        totalFileCountdownLatch = new CountDownLatch(totalFile.intValue());
         letterCountDir("/Users/valtechwh/Documents/N132","pdf",false);
+        totalFileCountdownLatch.await();
         System.out.println("--------------------------【END】Letter count:["+totalLetter+"],cost ["+(System.currentTimeMillis()-startTime)+"]ms");
+        threadPoolExecutor.shutdown();
+
     }
 
 
     public static void letterCountDir(String path,String format,boolean justCountFile){
         File file = new File(path);
-        //非空文件夹
-        if(file.isDirectory() && file.listFiles()!=null){
-            for(File f : file.listFiles()){
-                letterCountDir(f.getAbsolutePath(),format,justCountFile);
-            }
-        }else if(file.isFile() && getFileFormat(file.getAbsolutePath()).equals(format)){
-            if(justCountFile){
-                //仅统计文件个数
-                totalFile.incrementAndGet();
-            }else{
-                //如果是指定格式的文件，统计字数
-                letterCount(file);
+        //只遍历文件夹
+        if(!file.isDirectory()){return;}
+        if(file.listFiles()!=null){
+            for(File file1:file.listFiles()){
+                if(file.getName().equals("zh") && file1.isFile()
+                        && getFileFormat(file1.getAbsolutePath()).equals(format)){
+                    if(justCountFile){
+                        totalFile.incrementAndGet();
+                    }else{
+                        threadPoolExecutor.execute(()->{
+                            letterCount(file1);
+                            totalFileCountdownLatch.countDown();
+                        });
+                    }
+                }else if(file1.isDirectory()){
+                    letterCountDir(file1.getAbsolutePath(),format,justCountFile);
+                }
+
             }
         }
     }
 
     public static long letterCount(File file){
-        //System.out.print(file.getAbsolutePath()+",");
         String suffix = getFileFormat(file.getAbsolutePath());
         switch(suffix){
             case "pdf": return letterCountPDF(file.getAbsolutePath());
@@ -59,31 +75,37 @@ public class FileUtils {
             return countText(reader);
         } catch (IOException e) {
             e.printStackTrace();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
         return -1;
     }
 
-    public static Integer countText(PdfReader reader) {
+    public static String replaceUselessLetters(String content){
+        return content
+                .replaceAll("[^\\u4E00-\\u9FA5|^\\u2F00-\\u2FD5]","");
+    }
+
+    public static Integer countText(PdfReader reader) throws InterruptedException {
         try {
-            int characterCount = 0;
+            AtomicInteger characterCount = new AtomicInteger();
 
             PdfDocument pdfDoc = new PdfDocument(reader);
             // get the number of pages in PDF
             int noOfPages = pdfDoc.getNumberOfPages();
             for (int i = 1; i <= noOfPages; i++) {
-                //TODO 多线程
-                // Extract content of each page
-                String contentOfPage = PdfTextExtractor.getTextFromPage(pdfDoc.getPage(i));
-                //TODO 替换无用字符
-                contentOfPage = contentOfPage.replaceAll("\\p{C}", "").replaceAll(" ", "");
-                //统计数量
-                characterCount += contentOfPage.length();
+                    // Extract content of each page
+                    String contentOfPage = PdfTextExtractor.getTextFromPage(pdfDoc.getPage(i));
+                    //TODO 替换无用字符
+                    contentOfPage = replaceUselessLetters(contentOfPage);
+                    //统计数量
+                    characterCount.addAndGet(contentOfPage.length());
             }
             pdfDoc.close();
-            //System.out.print(characterCount+",");
-            totalLetter.addAndGet(characterCount);
+            //单个文件全部统计完成后再进行汇总
+            totalLetter.addAndGet(characterCount.longValue());
             System.out.println("Progress:"+totalFile+"/"+totalFileRead.incrementAndGet()+"("+NumberUtils.calculatePercentage(totalFileRead.longValue(),totalFile.longValue())+")");
-            return characterCount;
+            return characterCount.intValue();
         } finally {
             try {
                 if (reader != null) {
