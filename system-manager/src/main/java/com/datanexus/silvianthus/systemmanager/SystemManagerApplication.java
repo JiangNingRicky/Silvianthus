14a1361fac5e4f204ed2dcda35dbe19a89000ee7
ningj <jiangning_ricky@163.com>
2023-03-03 00:10:33 +0800

    First Commit 1. System manager 2. Task Manager

---
+++ system-manager/src/main/java/com/datanexus/silvianthus/systemmanager/SystemManagerApplication.java
@@ -0,0 +1,13 @@
+package com.datanexus.silvianthus.systemmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SystemManagerApplication.class, args);
+    }
+
+}
---
+++ system-manager/src/test/java/com/datanexus/silvianthus/systemmanager/SystemManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datanexus.silvianthus.systemmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
